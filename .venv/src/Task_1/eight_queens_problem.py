0a0baaccb61dd8a616bdffeefb4647d946da830b
CodingToffee <nicolasharrje@yahoo.de>
2024-06-04 17:05:14 +0200

    Added print_heuristics_all_actions

--- .venv/src/Task_1/eight_queens_problem.py
+++ .venv/src/Task_1/eight_queens_problem.py
@@ -1,32 +1,23 @@
 import random
 
+
 class EightQueensProblem:
     def __init__(self, transition_model, initial_state: [int] = None):
         self.initial_state: [int] = initial_state or [random.randint(0, 7) for i in range(8)]
         self.state: [int] = self.initial_state.copy()
         self.transition_model = transition_model
 
-    def horizontal_hit(self):
+    def heuristic(self, state):
         h = 0
-        for queen in self.state:
-            # get number of times, another queen is in the same row
-            h += self.state.count(queen) - 1
-
-        return h
-
-    def diagonal_hit(self):
-        h = 0
-        for index, queen in enumerate(self.state):
-            for other_index, other_queen in enumerate(self.state):
+        for index, queen in enumerate(state):
+            h += state.count(queen) - 1
+            for other_index, other_queen in enumerate(state):
                 difference = abs(index - other_index)
                 if difference != 0:
                     if queen - difference == other_queen or queen + difference == other_queen:
                         h += 1
 
         return h
-
-    def heuristic(self):
-        return self.horizontal_hit() + self.diagonal_hit()
 
     def print_state(self):
         print("________________")
@@ -43,5 +34,30 @@ class EightQueensProblem:
             line += "|\n"
         print(line)
 
+    def heuristics_all_actions(self):
+        actions = []
+        for i in range(8):
+            column = []
+            for j in range(8):
+                new_state = self.state.copy()
+                new_state[i] = j
+                column.append((new_state, self.heuristic(new_state)))
 
+            actions.append(column)
+        return actions
 
+    def print_heuristics_all_actions(self):
+        line = ""
+        for index, column in enumerate(self.heuristics_all_actions()):
+            for state, heuristic in column:
+                if state[index] == self.state[index]:
+                    line += "|QQ"
+                else:
+                    if heuristic % 10 == 0:
+                        heuristic = str(0) + str(heuristic)
+                    else:
+                        heuristic = str(heuristic)
+                    line += "|" + heuristic
+            line += "|\n"
+
+        print(line)
--- .venv/src/Task_1/test.py
+++ .venv/src/Task_1/test.py
@@ -1,8 +1,6 @@
 from eight_queens_problem import *
 
-
-
-
 b = EightQueensProblem("genetic", [0, 1, 2, 3, 4, 5, 6, 7])
 
 b.print_state()
+b.print_heuristics_all_actions()
